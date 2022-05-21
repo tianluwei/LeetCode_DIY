f8391822a2687b3b47f30c9150a63ffe189bbf8c
tianluwei <2943873912@qq.com>
2022-05-21 08:52:36 +0800

    每日一刷，做了二分相关，掌握程度：好

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ###Array
 - BinarySearch [704](https://leetcode.cn/problems/binary-search/)
 - 搜索插入位置 SearchInsert [35](https://leetcode.cn/problems/search-insert-position/)
+- 查找元素第一个和最后一个范围SearchRange [34](https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/)
+- 一个数的平方根sqrt [69](https://leetcode.cn/problems/sqrtx/)
+- 
--- src/array/SearchInsert35.java
+++ src/array/SearchInsert35.java
@@ -1,30 +1,64 @@
 package array;
 
 public class SearchInsert35 {
+    public static void main(String[] args) {
+        int[] arr={1,3,5,6};
+        int i = new SearchInsert35().searchInsert(arr, 7);
+        System.out.println(i);
+    }
 
 //    我自己的写法，pass
+//    public int searchInsert(int[] nums, int target) {
+//        int left=0;
+//        int right=nums.length-1;
+//        int res=0;
+//         if(nums[right]<target){
+//             return nums.length;
+//         }
+//         if(nums[left]>target){
+//             return 0;
+//         }
+//        while(left<=right){
+//            int mid=(left+right)/2;
+//            if(nums[mid]<target){
+//                left=mid+1;
+//                res=left;
+//            }else if(nums[mid]>target){
+//                right=mid-1;
+//                res=mid;
+//            }else{
+//                return mid;
+//            }
+//        }
+//        return res;
+//    }
+//    暴力法
+//    public int searchInsert(int[] nums, int target) {
+//        for(int i=0;i<nums.length;i++){
+//            if(nums[i]>=target){
+//                return i;
+//            }
+//        }
+//        return nums.length;
+//    }
+
+
+//    a better solution
     public int searchInsert(int[] nums, int target) {
         int left=0;
         int right=nums.length-1;
-        int res=0;
-         if(nums[right]<target){
-             return nums.length;
-         }
-         if(nums[left]>target){
-             return 0;
-         }
+        int mid=0;
         while(left<=right){
-            int mid=(left+right)/2;
+            mid=left+(right-left)/2;
             if(nums[mid]<target){
                 left=mid+1;
-                res=left;
             }else if(nums[mid]>target){
                 right=mid-1;
-                res=mid;
             }else{
                 return mid;
             }
         }
-        return res;
+        return right+1;
+//        return left;  //**也是OK的**
     }
 }
